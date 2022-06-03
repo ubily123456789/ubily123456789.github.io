60e1371ff4c864ece58ba2cb5f3e6787f9ef4605
ubily123456789 <103503468+ubily123456789@users.noreply.github.com>
2022-06-03 18:10:02 +0100

    dev

--- baseball_game.py
+++ baseball_game.py
@@ -7,24 +7,40 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("baseball game!")
 FPS = 60
 
+# the thing
+x = 100
+bat = pygame.Rect(x, 450, 200, 50)
+
 # colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+BACKGROUND_COLOUR = (0, 222, 222)
+YELLOW = (255, 255, 0)
 
 # define functions
+def move():
+    global x
+    keys_press = pygame.key.get_pressed()
+    if keys_press[pygame.K_LEFT]:
+        x = x+2
+
 def draw():
-    WIN.fill(WHITE)
+    WIN.fill(BACKGROUND_COLOUR)
+    pygame.draw.rect(WIN, BLACK, bat)
     pygame.display.flip()
 
 # game loop
 def main():
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
+        move()
         draw()
+
 
     pygame.quit()
 
