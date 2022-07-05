0a77e946208766f8eb2617169dab73d15dd2c508
ubily123456789 <103503468+ubily123456789@users.noreply.github.com>
2022-07-05 20:36:58 +0100

    dev

--- p/main.js
+++ p/main.js
@@ -2,8 +2,8 @@ for(let i = 0; i <= 10; i++){
     console.log(`for loop number: ${i}`);
 }
 
-let i = 0;
-while (1 < 10){
-    console.log(`while loop numbers ${i}`);
-    i++;
-}
+// let i = 0;
+// while (1 < 10){
+    // console.log(`while loop numbers ${i}`);
+    // i++;
+// }
