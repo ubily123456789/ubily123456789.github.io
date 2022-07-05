20b6ad22d1a265769887f1e84cd50baaefed3cbf
ubily123456789 <103503468+ubily123456789@users.noreply.github.com>
2022-07-05 20:31:40 +0100

    dev

---
+++ p/main.js
@@ -0,0 +1,9 @@
+for(let i = 0; i <= 10; i++){
+    console.log(`for loop number: ${i}`);
+}
+
+let i = 0;
+while (1 < 10){
+    console.log(`while loop numbers ${i}`);
+    i++;
+}
--- this.py
+++
@@ -1,2 +0,0 @@
-for i in range(11):
-    print("for loop number: ",i)
