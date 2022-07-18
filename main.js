5621983ec4a7abce9610b28caf2f98774e11b392
ubily123456789 <103503468+ubily123456789@users.noreply.github.com>
2022-07-18 13:27:03 +0100

    dev

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-const nameInput = document.querySelector('.dev');
-const myForm = document.querySelector('.sub');
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+const linkText = document.createTextNode("Iceland");
+
 
 myForm.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
     e.preventDefault();
 
-    if (nameInput.value === 'google') {
-        alert('hello')
+    if (nameInput.value === 'Google') {
+        alert('you clicked me :)');
+    }
+
+    // testing
+
+    if (nameInput.value === 'testing') {
+        alert('nothing for testing right now');
     }
 }
