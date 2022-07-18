0e69d86fbbea3798fb639f4cc88d4d3247d730d5
ubily123456789 <103503468+ubily123456789@users.noreply.github.com>
2022-07-18 22:22:35 +0100

    dev

--- main.js
+++ main.js
@@ -2,22 +2,28 @@ const myForm = document.querySelector('#my-form');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const msg = document.querySelector('.msg');
-const userList = document.querySelector('#users');
-const linkText = document.createTextNode("Iceland");
+const userList = document.querySelector('.users');
+const linkText = document.createTextNode("dev");
 
 
 myForm.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    if (nameInput.value === 'Google') {
-        alert('you clicked me :)');
+  if (nameInput.value === 'Google') {
+      alert('you clicked me :)');
     }
 
-    // testing
+  // testing
 
-    if (nameInput.value === 'testing') {
-        alert('nothing for testing right now');
-    }
+  if (nameInput.value === 'ReAsEaRcH StYdY') {
+    const a = document.createElement('a');
+    a.appendChild(linkText);
+    a.href = "dev.html";
+
+    userList.appendChild(a);
+    userList.classList.add('ull');
+    a.classList.add('lnk');
+  }
 }
--- python/baseball_game.py
+++ python/baseball_game.py
@@ -1,5 +1,1 @@
-# for testing
-import pygame
-
-screenA = pygame.display.set_mode((300,480), 0, 32)
-screenB = pygame.display.set_mode((500,480), 0, 32)
+print(0.1 + 0.2)
