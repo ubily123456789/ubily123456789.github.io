73babda975453b6832c68be4df2d8f206b6e5725
ubily123456789 <103503468+ubily123456789@users.noreply.github.com>
2022-07-20 19:57:40 +0100

    dev

--- games/games.js
+++ games/games.js
@@ -11,12 +11,20 @@ console.log(`${up} : ${down}`);
 
 // selecting the html
 const number = document.querySelector('#number');
+const userList = document.querySelector('#myForm');
+const myForm = document.querySelector('#my-form');
+const guess = document.querySelector('.btnn');
 const linkText = document.createTextNode(num);
 
 // ------------------------------------------------------------------------------------------------------------------------------------
 
+myForm.addEventListener('submit', onSubmit);
 
+function onSubmit(e) {
+    e.preventDefault();
 
+    console.log(guess.value);
+}
 
 
 // ------------------------------------------------------------------------------------------------------------------------------------
