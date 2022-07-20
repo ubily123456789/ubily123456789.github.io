e5ac57fb44638e5cba10ecf0e2963af697d9cf01
ubily123456789 <103503468+ubily123456789@users.noreply.github.com>
2022-07-20 19:24:13 +0100

    dev

--- games/games.js
+++ games/games.js
@@ -1,9 +1,27 @@
+const difam = 10; // going to be input
+const difnum = Math.floor(Math.random() * difam) + 1;
+
+// the random number
+
+const num = Math.floor(Math.random() * 9999) + 1;
+
+const up = num + difnum
+const down = num - difnum
+console.log(`${up} : ${down}`);
+
+// selecting the html
 const number = document.querySelector('#number');
 const linkText = document.createTextNode(num);
 
-var num = Math.floor(Math.random() * 9999) + 1;
-console.log(num);
+// ------------------------------------------------------------------------------------------------------------------------------------
 
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------------------------
+
+// transforming the text
 const a = document.createElement('h3');
 a.appendChild(linkText);
 
