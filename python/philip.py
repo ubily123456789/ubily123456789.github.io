af843bc1eb9949053fbee683fd5f68717cce2b2e
Macleod <mmd984@abdn.ac.uk>
2022-08-06 21:56:03 +0100

    dev

--- python/philip.py
+++ python/philip.py
@@ -3,40 +3,17 @@ import turtle
 phil = turtle.Pen()
 phil.shape("turtle")
 
-def fname(sides, size):
-    for i in range():
-        pass
-
-# fname(7, 70)fd AD F
-
-# phil.circle(size/2, 180)
-# half a circle ^
-#               |
-
 #-----------------------------------------------------------------------------------------------------------
 
-def shape(sides, size):
-    if sides == 2:
-        phil.forward(size)
-        phil.left(90)
-        phil.circle(size/2, 180)
-        phil.left(90)
-    elif sides == 1:
-        phil.circle(size/2)
-    else:
-        for i in range(sides):
-            phil.forward(size)
-            phil.left(360/sides)
-
-
-
-shape(1, 100)
+phil.forward(999)
 
 # Philip
 
 # 1st B
 # 2nd A+
-# 3rd ?
-# current grade: A
+# 3rd B+
+# 4th B-
+
+# current grade: B+
 
 turtle.mainloop()
