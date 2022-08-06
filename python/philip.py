ce3a8305058fd28390f8334184e49eee7d10b40d
ubily123456789 <103503468+ubily123456789@users.noreply.github.com>
2022-08-06 12:48:27 +0100

    dev

--- python/philip.py
+++ python/philip.py
@@ -1,11 +1,42 @@
 # Kenneth
+import turtle
+phil = turtle.Pen()
+phil.shape("turtle")
 
-var = 0
-while var != 10:
-    print("hello world")
+def fname(sides, size):
+    for i in range():
+        pass
+
+# fname(7, 70)fd AD F
+
+# phil.circle(size/2, 180)
+# half a circle ^
+#               |
 
 #-----------------------------------------------------------------------------------------------------------
 
+def shape(sides, size):
+    if sides == 2:
+        phil.forward(size)
+        phil.left(90)
+        phil.circle(size/2, 180)
+        phil.left(90)
+    elif sides == 1:
+        phil.circle(size/2)
+    else:
+        for i in range(sides):
+            phil.forward(size)
+            phil.left(360/sides)
 
+
+
+shape(1, 100)
 
 # Philip
+
+# 1st B
+# 2nd A+
+# 3rd ?
+# current grade: A
+
+turtle.mainloop()
