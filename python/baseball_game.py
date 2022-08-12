73ae6e3d9f617f9e28e4409099e1d82d34360121
ubily123456789 <103503468+ubily123456789@users.noreply.github.com>
2022-08-12 14:30:15 +0100

    dev

--- python/baseball_game.py
+++ python/baseball_game.py
@@ -17,13 +17,15 @@ def power(num, power):
 
 def factorial(num):
     other = 0
-    ans = 0
+    ans = 1
     for i in range(num):
         other = other + 1
-        print(i+1)
-        ans = ans * num
-
+        print(other)
+        ans = ans * other
     print("")
     print(ans)
 
-factorial(4)
+def root(num):
+    power(num, 0.5)
+
+factorial(12)
