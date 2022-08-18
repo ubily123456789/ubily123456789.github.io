9ddb5b5a48a01a490853c1e345c9f6c2b5f46d9b
ubily1234 <103503468+ubily123456789@users.noreply.github.com>
2022-08-18 22:03:16 +0100

    dev

--- python/multi.py
+++ python/multi.py
@@ -1,26 +1,5 @@
-import pygame
+# input("Pick a number: ")
+print("nice choice you won")
+import random
 
-def highscore():
-    pygame.font.init()
-    background_colour = (255,255,255)
-    (width, height) = (600, 600)
-
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption('Tutorial 1')
-    screen.fill(background_colour)
-
-    pygame.display.flip()
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        myfont = pygame.font.SysFont("monospace", 75)
-        BLACK = (0, 0, 0)
-        label = myfont.render("Tutorial 1", 1, BLACK)
-        screen.blit(label, (0, 10))
-        pygame.display.update()
-
-highscore()
+random._test()DZX           ``
