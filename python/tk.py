75b9873def235fd2ad6976253efc04c19e8716a2
ubily1234 <103503468+ubily123456789@users.noreply.github.com>
2022-08-20 21:15:05 +0100

    dev

--- python/tk.py
+++ python/tk.py
@@ -5,7 +5,8 @@ canvas = Canvas(tk, width=500, height=400)
 tk.title("hello")
 canvas.pack()
 
-canvas.create_rectangle(10, 10, 100, 100, fill="yellow")
-canvas.create_oval(100, 100, 200, 300)
+canvas.create_rectangle(200, 10, 100, 100, fill="yellow")
+canvas.create_oval(100, 100, 200, 300, fill="black")
+canvas.create_polygon(10, 10, 100, 100, 0, 100, fill="blue")
 
 canvas.mainloop()
