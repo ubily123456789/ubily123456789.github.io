006083ab949e9f0ca4e0aa5653ae1b145718eb05
ubily1234 <103503468+ubily123456789@users.noreply.github.com>
2022-08-29 07:06:35 +0100

    dev

--- python/philip.py
+++ python/philip.py
@@ -1,16 +1,11 @@
 # Kenneth
-import turtle
-phil = turtle.Pen()
-phil.shape("turtle")
+
+
 
 #-----------------------------------------------------------------------------------------------------------
 
-for i in range(4):
-    phil.forward(100)
-    phil.left(90)
+
 
 # Philip
 
 # Grade
-
-turtle.mainloop()
