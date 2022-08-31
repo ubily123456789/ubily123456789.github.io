3d38c414690e94cee2fc9f0747ac1272c37b4021
ubily1234 <103503468+ubily123456789@users.noreply.github.com>
2022-08-31 16:32:06 +0100

    dev

--- python/multi.py
+++ python/multi.py
@@ -1,5 +1,7 @@
+# a game for me and Philip
 import pygame
 
+# window stuff
 WIDTH, HEIGHT = 1200, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("K & P")
@@ -10,11 +12,22 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 # score
+a = 0
 k = 0
 p = 0
 
+# text
+pygame.font.init()
+myfont = pygame.font.SysFont("comicsans", 75)
+k_s = myfont.render(str(k), 1, BLACK)
+p_s = myfont.render(str(p), 1, BLACK)
+
 def draw():
+    p_s = myfont.render(str(p), 1, BLACK)
+    k_s = myfont.render(str(k), 1, BLACK)
     WIN.fill(WHITE)
+    WIN.blit(k_s, (50, 50))
+    WIN.blit(p_s, (1100, 50))
     pygame.display.flip()
 
 def score():
@@ -30,9 +43,13 @@ def main():
                 run = False
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                    global p
-                    p += 1
+                if event.key == pygame.K_l: global p; p += 1
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a: global k; k += 1
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s: k += 2
         draw()
 
     pygame.quit()
--- python/philip.py
+++ python/philip.py
@@ -1,6 +1,49 @@
 # Kenneth
 
+import pygame
+import os
+pygame.font.init()
 
+FONT = pygame.font.SysFont('comicsans', 100)
+
+SCREENX = 1700
+SCREENY = 900
+
+WHITE = (255, 255, 255)
+
+x = 0
+TEXT = FONT.render(str(x), 1, (255, 255, 255))
+
+WIN = pygame.display.set_mode((SCREENX, SCREENY))
+BACKGROUND = pygame.transform.scale(pygame.image.load(
+    os.path.join('Assets', 'space.png')), (SCREENX, SCREENY))
+
+
+def draw_window():
+    WIN.blit(BACKGROUND, (0, 0))
+    TEXT = FONT.render(str(x), 1, (255, 255, 255))
+    WIN.blit(TEXT, ((SCREENX - TEXT.get_width()) /
+         2, (SCREENY - TEXT.get_height()) / 2))
+    pygame.display.update()
+
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    global x
+                    x += 1
+
+        draw_window()
+    pygame.quit
+
+
+if __name__ == '__main__':
+    main()
 
 #-----------------------------------------------------------------------------------------------------------
 
