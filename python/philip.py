ebc5f382a974e07a3f5673a6da4bf24250fb4b08
Macleod <mmd984@abdn.ac.uk>
2022-10-24 20:17:33 +0100

    dev

--- python/philip.py
+++ python/philip.py
@@ -1,39 +1,10 @@
 # Kenneth
 
-"""
-make a function for maths
-"""
-
-"""def func(num1, num2, type="add"):
-    # do maths
-    if type == "add":
-        print(num1 + num2)
-    if type == "times":
-        pass"""
-
-
-
-class Person():
-    def __init__(self, age, name):
-        self.age = age
-        self.name = name
-
-    def name(self):
-        print(self.name)
-
-    def age(self):
-        print(self.age)
-
-
-henry = Person(20, "henry")
-
-henry.age()
-
 
 
 #-----------------------------------------------------------------------------------------------------------
 
-
+print("hello world")
 
 # Philip
 
